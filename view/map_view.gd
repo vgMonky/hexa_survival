c8d76079c58ab3a41c0a5678b3ac7296b0b0c43f
Victor Gou <victorgou.design@gmail.com>
2024-11-26 16:08:07 -0300

    map_view draws and manages the view fully

--- view/map_view.gd
+++ view/map_view.gd
@@ -1,6 +1,23 @@
 class_name MapView
 extends Node2D
 
+# Inner class for hex visualization
+class HexLocation extends Node2D:
+	const HEX_SIZE = 32  # Distance from center to corner
+	const WIDTH_MULTIPLIER = 0.866  # sqrt(3)/2
+
+	func _draw() -> void:
+		var points = []
+		for i in range(6):
+			var angle = i * PI / 3.0
+			var x = cos(angle) * HEX_SIZE
+			var y = sin(angle) * HEX_SIZE
+			points.append(Vector2(x, y))
+		
+		draw_colored_polygon(points, Color.white)
+		draw_polyline(points + [points[0]], Color.black, 1.0)
+
+# Map View properties
 var game_map: GameMap
 var hex_locations = {}
 
@@ -29,11 +46,12 @@ func _create_visual_map() -> void:
 	hex_locations.clear()
 	
 	for pos in game_map.hex_data.keys():
-		var hex = HexLocationView.new()
+		var hex = HexLocation.new()
 		add_child(hex)
 		
-		var pixel_x = HexLocationView.HEX_SIZE * (3.0/2.0 * pos.x)
-		var pixel_y = HexLocationView.HEX_SIZE * (sqrt(3)/2.0 * pos.x + sqrt(3) * pos.y)
+		# Convert axial coordinates to pixel position
+		var pixel_x = HexLocation.HEX_SIZE * (3.0/2.0 * pos.x)
+		var pixel_y = HexLocation.HEX_SIZE * (sqrt(3)/2.0 * pos.x + sqrt(3) * pos.y)
 		
 		hex.position = Vector2(pixel_x, pixel_y)
 		hex_locations[pos] = hex
