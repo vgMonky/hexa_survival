e1e3a518d9d7fa81336fbdaf9847f5065a79da28
Victor Gou <victorgou.design@gmail.com>
2024-11-27 16:13:37 -0300

    info_ui fixed

--- main.gd
+++ main.gd
@@ -3,14 +3,19 @@ extends Node2D
 func _ready() -> void:
 	var state_manager = StateManager.new()
 	add_child(state_manager)
-	state_manager.initialize(5, 5)
+	state_manager.initialize(8, 5)
 	
 	state_manager.apply_state_change({
 		"type": "add_team",
 		"team_name": "Red Team",
 		"team_color": Color.red
 	})
-	
+	state_manager.apply_state_change({
+		"type": "add_team",
+		"team_name": "Vic Team",
+		"team_color": Color.blue
+	})
+
 	var map_view = MapView.new()
 	add_child(map_view)
 	map_view.initialize(state_manager)
--- view/ui/info_ui.gd
+++ view/ui/info_ui.gd
@@ -3,23 +3,33 @@ class_name InfoUI
 extends UIBox
 
 var map_info: Label
-var hex_info: Label
-var team_info: Label
+var team_info: VBoxContainer
 
-func _init().("Game Info", Control.PRESET_TOP_RIGHT) -> void:
+func _init(title: String = "", pos_preset: int = Control.PRESET_TOP_RIGHT).(title, pos_preset) -> void:
 	set_margins(-200, 10, -10)
-	
 	map_info = Label.new()
-	hex_info = Label.new()
-	team_info = Label.new()
-	
+	team_info = VBoxContainer.new()
 	content.add_child(map_info)
-	content.add_child(hex_info)
+	content.add_child(HSeparator.new())
 	content.add_child(team_info)
-	
-	map_info.text = "Hover over map for info"
 
 func update_state_info(state: GameState) -> void:
-	map_info.text = "Map Info:\n"
-	map_info.text += "Size: %d x %d\n" % [state.map_data.width, state.map_data.height]
-	map_info.text += "Teams: %d" % len(state.teams.team_data)
+	map_info.text = "Map Size: %d x %d" % [state.map_data.width, state.map_data.height]
+	map_info.text += "\nHexes: %d" % len(state.map_data.hexes)
+
+	# Clear the previous team info
+	for child in team_info.get_children():
+		child.queue_free()
+
+	# Display the list of teams and their colors
+	var team_label = Label.new()
+	team_label.text = "Teams: %d" % len(state.teams.team_data)
+	team_info.add_child(team_label)
+
+
+	for team_name in state.teams.team_data:
+		var team_data = state.teams.team_data[team_name]
+		team_label = Label.new()
+		team_label.text = team_name
+		team_label.modulate = team_data.color
+		team_info.add_child(team_label)
