f7552c8ef3f52b250f56a2b4913a44cf16c0ead6
vgMonky <victorgou.design@gmail.com>
2024-12-01 23:48:37 -0300

    biomes now have % chance to spawn

--- hex_survival/game/data/biome_types.gd
+++ hex_survival/game/data/biome_types.gd
@@ -1,4 +1,3 @@
-# game/data/biome_types.gd
 class_name BiomeTypes
 extends Reference
 
@@ -6,6 +5,7 @@ const TYPES = {
 	"WOODS": {
 		"color": Color.darkgreen,
 		"walkable": true,
+		"spawn_chance": 0.6,  #chance to spawn
 		"resources": {
 			"wood": 0.9  # 90% chance
 		}
@@ -13,6 +13,7 @@ const TYPES = {
 	"CAVE": {
 		"color": Color.lightslategray,
 		"walkable": true,
+		"spawn_chance": 0.2,  # chance to spawn
 		"resources": {
 			"rock": 0.9,  # 90% chance
 			"iron": 0.2   # 20% chance
@@ -21,19 +22,32 @@ const TYPES = {
 	"DESERT": {
 		"color": Color.goldenrod,
 		"walkable": true,
+		"spawn_chance": 0.3, 
 		"resources": {}  # no resources
 	},
 	"WATER": {
 		"color": Color.darkblue,
 		"walkable": false,
+		"spawn_chance": 0.3, 
 		"resources": {}  # no resources
 	}
 }
 
-# Helper function to get random biome type
+# Helper function to get random biome type using spawn chances
 static func get_random_type() -> String:
-	var types = TYPES.keys()
-	return types[randi() % types.size()]
+	var total_chance := 0.0
+	for type in TYPES:
+		total_chance += TYPES[type].spawn_chance
+	
+	var roll = randf() * total_chance
+	var current_total := 0.0
+	
+	for type in TYPES:
+		current_total += TYPES[type].spawn_chance
+		if roll <= current_total:
+			return type
+	
+	return "DESERT"  # Fallback
 
 # Helper function to get biome data
 static func get_data(biome_type: String) -> Dictionary:
