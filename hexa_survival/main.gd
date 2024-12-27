956e6962cb529d5566d705ceb473dcd55a0bed8d
Victor Gou <victorgou.design@gmail.com>
2024-12-26 22:06:56 -0300

    base state system created

--- hexa_survival/main.gd
+++ hexa_survival/main.gd
@@ -1,3 +1,15 @@
 extends Node2D
 
-
+func _ready():
+	# Create an instance of StateManager
+	var state_manager = StateManager.new()
+	var state_manager_two = StateManager.new()
+	
+	# Simulate an event
+	var test_event = Event.new()
+	state_manager.change_game_state(test_event)
+	
+	# Simulate a query
+	var test_query = Query.new()
+	var result = state_manager.query_game_state(test_query)
+	print("Query result:", result)
